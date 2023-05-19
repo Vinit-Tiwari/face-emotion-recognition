1133e856b62651c8ec71eef21e20c2593b180c48
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2023-05-19 09:13:52 +0530

    Completed project

--- main.py
+++ main.py
@@ -30,15 +30,17 @@ faceCascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
+cap.set(3, 640)
+cap.set(4, 480)
 
 if not cap.isOpened():
     raise IOError("Cannot open webcam!")
 
 while True:
     ret, frame = cap.read()
-    result = DeepFace.analyze(frame, actions=['emotions'])
+    result = DeepFace.analyze(
+        frame, actions=['emotion'], enforce_detection=False)
+    print(result)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray, 1.1, 4)
@@ -47,12 +49,12 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     font = cv2.FONT_HERSHEY_PLAIN
-    cv2.putText(frame, result['dominant_emotion'],
+    cv2.putText(frame,  result[0]['dominant_emotion'],
                 (0, 50), font, 1, (0, 0, 255), 2, cv2.LINE_4)
 
-    cv2.imshow('Original video',frame)
-    
-    if cv2.waitKey(2) & 0xFF==ord('q'):
+    cv2.imshow('Original video', frame)
+
+    if cv2.waitKey(2) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
